968621a827440996772eab3c9c9b4672b310e385
Edwin Nchoga <edwinchoga@gmail.com>
2022-09-08 04:52:08 -0700

    added task 4 - 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*A simple program to print a message 
+ * to screen.
+ * Returns 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
+	
