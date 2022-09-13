799ec7fe5fd5c2f20bb82e7b6aa9bf73cb48bcb0
Edwin Nchoga <edwinchoga@gmail.com>
2022-09-13 09:22:49 -0700

    added task 1-4

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
