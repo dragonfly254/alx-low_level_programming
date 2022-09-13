11916b0d2075d9ca53ccf2de5cc8d3e7d917565b
Edwin Nchoga <edwinchoga@gmail.com>
2022-09-13 09:32:26 -0700

    corrected task 1 and 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 97; i <= 122; i++)
+		for (j = 97; j <= 122; j++)
 		{
-			_putchar(i);
+			_putchar(j);
 		}
 		_putchar('\n');
 	}
