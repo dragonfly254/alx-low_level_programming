a110f7450be318559de1fafbc6bf9b998e1a894e
Edwin Nchoga <edwinchoga@gmail.com>
2022-10-11 06:58:13 -0700

    added tasks 1 - 5

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints dog info.
+ *
+ * @d: the dog object to be printed.
+ *
+ * Return: none.
+ */
+void print_dog(struct dog *d)
+{
+	if (fd)
+	{
+		if (!(d.name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d.name);
+		printf("Age: %f\n", d->age);
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
