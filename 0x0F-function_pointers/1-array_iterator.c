d1632639638cb7433dd043e10619b0e0f2ea5ad7
Edwin Nchoga <edwinchoga@gmail.com>
2022-10-13 03:12:43 -0700

    added task 1 - 4 of the task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointer.h"
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array.
+ *
+ * @array: input array.
+ * @size: size of the array.
+ * @action: function pointer.
+ *
+ * Return: none.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array && action)
+	{
+		for (;i < size; i++)
+			action(array[1]);
+	}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer.
+ *
+ * @array: input array.
+ * @size: the size of the integer.
+ * @cmp: funtion pointer.
+ *
+ * Return: returns the index of the first element for which the cmp
+ * function does not return 0.If no element matches, return -1 or 
+ * If size <= 0, return -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+	}
+	return (-1);
+}
