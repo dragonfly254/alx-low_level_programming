e2176415e7cd0881943905aa939ef14154cce41c
Edwin Nchoga <edwinchoga@gmail.com>
2022-10-14 08:18:17 -0700

    debuged task 0 & 3

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "variadic_fuctions.h"
+#include "variadic_functions.h"
 /**
  * sum_them_all - returns the sum of all its parameters.
  *
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,8 +22,7 @@ void print_all(const char * const format, ...)
 			{
 				printf(", ");
 				break;
-			}
-			j++;
+			}j++;
 		}
 		switch (format[i])
 		{
