ceefaceb93a09fe43ab5af732a2300e6a86c89b2
Edwin Nchoga <edwinchoga@gmail.com>
2022-11-04 00:22:09 -0700

    debugged task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it into the POSIX standard output.
+ * read_textfile - reads a text file and prints it into the
+ * POSIX standard output.
  * @filename: name of file to read from.
  * @letters: the number of letters to read.
  *
  * Return: actual number of letters it could read and print
  *        0 if the file can not be opened or read,
- *          if filename is NULL, or 
+ *          if filename is NULL, or
  *          if write fails or does not write the expected
  *          amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char* buff;
+	char *buff;
 	ssize_t nread, nwrite;
 
 	if (!filename)
@@ -22,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (fd == -1)
 		return (0);
-	
+
 	buff = malloc(letters);
 
 	nread = read(fd, buff, letters);
