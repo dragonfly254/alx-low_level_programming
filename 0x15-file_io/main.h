6f23ddfddfae4a72618a862f659ca07dcaba043f
Edwin Nchoga <edwinchoga@gmail.com>
2022-11-04 00:11:49 -0700

    debugged task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
