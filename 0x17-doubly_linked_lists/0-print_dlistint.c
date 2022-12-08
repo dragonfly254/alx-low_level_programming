163ce6bfe84a0c4519ffb3c2bdd1a1e06e6dc852
Edwin Nchoga <edwinchoga@gmail.com>
2022-12-08 10:49:45 -0800

    debugged task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,24 +8,19 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	if (!h)
-		return (0);
+	int count = 0;
 
-	int nnodes;
-	dlistint_t *curr;
+	if (h == NULL)
+		return (count);
 
 	while (h->prev != NULL)
 		h = h->prev;
 
-	curr = h;
-	nnodes = 0;
-
-	while (curr)
+	while (h != NULL)
 	{
-		printf("%d\n", curr->n);
-		nnodes++;
-		curr = curr->next;
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
 	}
-
-	return (nnodes);
+	return (count);
 }
