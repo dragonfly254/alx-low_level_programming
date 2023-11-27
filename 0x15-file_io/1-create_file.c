21a61a0725a7b86b89eada49e80e6a1308e67891
Edwin Nchoga <edwinchoga@mail.com>
2023-11-27 02:34:56 -0800

    debug 1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,17 +6,19 @@
  * @text_content: NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 if filename is null or if text_content
- * 	is null create empty file.
+ * is null create empty file.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, tlen;
 	ssize_t nwrite;
 
-	if (filename == NULL) return (-1);
+	if (filename == NULL)
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
-	if (fd == -1) return (-1);
+	if (fd == -1)
+		return (-1);
 
 	if (!text_content)
 	{
@@ -25,8 +27,9 @@ int create_file(const char *filename, char *text_content)
 	for (tlen = 0; text_content[tlen]; tlen++)
 		;
 
-	nwrite = write(fd,text_content, tlen);
-	if (nwrite == -1) return (-1);
+	nwrite = write(fd, text_content, tlen);
+	if (nwrite == -1)
+		return (-1);
 
 	close(fd);
 	return (1);
