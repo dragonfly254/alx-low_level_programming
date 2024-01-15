9a0e52f90a5b5d2f15ca7ee4673fdee020813dcc
Edwin Nchoga <edwinchoga@mail.com>
2024-01-15 08:11:20 -0800

    initial commit

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ *
+ * @s: memory area.
+ * @b: character to fill the memory area s with.
+ * @n: number of bytes to be filled.
+ *
+ * Return: memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int counter = 0;
+
+	for (; counter < n; counter++)
+	{
+		*(s + counter) = b;
+	}
+	return (s);
+}
